41efe1915e048d657adbab05b9867851e570adc4
DaniArman-blip <ramaini1005@gmail.com>
2026-09-15 14:39:19 +0700

    Delete bayar_dani.py

--- bayar_dani.py
+++
@@ -1,8 +0,0 @@
-harga = int(input("harga barang: "))
-diskon = int(input("diskon barang: "))
-jumlah = int(input("jumlah barang: "))
-
-total = harga * jumlah
-total = (harga * jumlah) / diskon 
-
-print("total pembayaran:", total)
